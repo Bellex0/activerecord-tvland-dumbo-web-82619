3c29ab6082d317e1f04176f10460651ca739fb45
Belle P <bellepx3@gmail.com>
2019-09-08 03:38:53 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,6 +1,6 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
    def change
-    create_table :networks do |t|
+    create_table :characters do |t|
       t.string :first_name
       t.string :last_name
     end
